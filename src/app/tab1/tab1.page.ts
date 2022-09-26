7fa17bed1bdb2a7cb3915d3e0704f0f7e10b1f93
timiliris <timothydetournay@gmail.com>
2022-09-26 19:33:35 +0200

    main

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import TCGdex from '@tcgdex/sdk';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor(
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const tcgdex = new TCGdex();
+    const cards = await tcgdex.fetch('cards');
+  }
 
 }
